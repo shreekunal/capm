bf035440711c7ceea250b594bfbcbab682cc9b4a
Kunal Singh <shrii.kunal@gmail.com>
2025-08-07 19:55:28 +0000

    Big Progress

--- project4/webapp/controller/View2.controller.js
+++ project4/webapp/controller/View2.controller.js
@@ -1,151 +1,89 @@
 sap.ui.define([
-  "sap/ui/core/mvc/Controller",
-  "sap/m/MessageBox",
-  "sap/ui/model/json/JSONModel"
-], function (Controller, MessageBox, JSONModel) {
-  "use strict";
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/Fragment",
+    "sap/m/MessageToast",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], function (Controller, Fragment, MessageToast, Filter, FilterOperator) {
+    "use strict";
 
-  return Controller.extend("project4.controller.View2", {
-
-    onInit: function () {
-      var oView = this.getView();
-
-      // Initialize JSON Model for UI state
-      var oUIModel = new JSONModel({
-        selectedDepartment: null,
-        selectedSupplier: null,
-        requestDate: null,
-        requestedBy: "",
-        product: []
-      });
-      oView.setModel(oUIModel, "ui");
-
-      // Load models for value help (optional based on dialogs)
-      var oODataModel = this.getOwnerComponent().getModel();
-
-      this._oDeptModel = new JSONModel();
-      oODataModel.read("/Departments", {
-        success: (oData) => this._oDeptModel.setData(oData.results),
-        error: () => MessageBox.error("Failed to load Departments.")
-      });
-
-      this._oSupplierModel = new JSONModel();
-      oODataModel.read("/Suppliers", {
-        success: (oData) => this._oSupplierModel.setData(oData.results),
-        error: () => MessageBox.error("Failed to load Suppliers.")
-      });
-
-      this._oProductModel = new JSONModel();
-      oODataModel.read("/Products", {
-        success: (oData) => this._oProductModel.setData(oData.results),
-        error: () => MessageBox.error("Failed to load Products.")
-      });
-    },
-
-    onDepartmentValueHelp: function () {
-      // Implementation (if required)
-    },
-
-    onSupplierValueHelp: function () {
-      // Implementation (if required)
-    },
-
-    onProductValueHelp: function (oEvent) {
-      // Implementation (if required)
-    },
-
-    onAddProductRow: function () {
-      var oUIModel = this.getView().getModel("ui");
-      var aProducts = oUIModel.getProperty("/product") || [];
-      aProducts.push({ productName: "", quantity: null, unit: "" });
-      oUIModel.setProperty("/product", aProducts);
-    },
-
-    onDeleteProductRow: function () {
-      var oTable = this.byId("productTable");
-      var aSelectedIndices = oTable.getSelectedIndices();
-
-      if (!aSelectedIndices.length) {
-        MessageBox.warning("Please select at least one product to delete.");
-        return;
-      }
-
-      var oUIModel = this.getView().getModel("ui");
-      var aProducts = oUIModel.getProperty("/product") || [];
-
-      aSelectedIndices.sort((a, b) => b - a).forEach(function (idx) {
-        aProducts.splice(idx, 1);
-      });
-
-      oUIModel.setProperty("/product", aProducts);
-      oTable.removeSelections();
-    },
-
-    onSubmit: function () {
-      var oModel = this.getView().getModel();
-      var oUIModel = this.getView().getModel("ui");
-      var oData = oUIModel.getData();
-
-      if (!oData.selectedDepartment || !oData.selectedSupplier || !oData.requestDate || !oData.requestedBy) {
-        MessageBox.error("Please fill all mandatory fields in General Information.");
-        return;
-      }
-
-      if (!oData.product.length) {
-        MessageBox.error("Please add at least one product.");
-        return;
-      }
-
-      for (var i = 0; i < oData.product.length; i++) {
-        var p = oData.product[i];
-        if (!p.productName || !p.quantity || !p.unit) {
-          MessageBox.error("All product fields are mandatory.");
-          return;
-        }
-        if (isNaN(p.quantity) || Number(p.quantity) <= 0) {
-          MessageBox.error("Quantity must be a positive number.");
-          return;
-        }
-      }
-
-      var oPayload = {
-        department: { cuid: oData.selectedDepartment.cuid },
-        supplier: { cuid: oData.selectedSupplier.cuid },
-        requestDate: oData.requestDate,
-        requestedBy: oData.requestedBy,
-        status: "New",
-        product: oData.product.map(function (p) {
-          return {
-            productName: p.productName,
-            quantity: Number(p.quantity),
-            unit: p.unit
-          };
-        }),
-        comments: ""
-      };
-
-      oModel.create("/ProcurementRequests", oPayload, {
-        success: function () {
-          MessageBox.success("Procurement request submitted successfully!");
-          oUIModel.setData({
-            selectedDepartment: null,
-            selectedSupplier: null,
-            requestDate: null,
-            requestedBy: "",
-            product: []
-          });
-          oModel.refresh(true);
+    return Controller.extend("project4.controller.View2", {
+        onInit: function () {
+            const oUIModel = new sap.ui.model.json.JSONModel({
+                selectedDepartmentName: "Select Department",
+                selectedSuppname: "Select Supplier"
+            });
+            this.getView().setModel(oUIModel, "ui");
         },
-        error: function (oError) {
-          MessageBox.error("Failed to submit procurement request: " + (oError.message || "Unknown error"));
+
+        // Department SelectDialog
+        onDeptHelpRequest: function () {
+            var oView = this.getView();
+
+            if (!this._pDeptDialog) {
+                this._pDeptDialog = Fragment.load({
+                    id: oView.getId(),
+                    name: "project4.fragment.DepartmentDialog",
+                    controller: this
+                }).then(function (oDialog) {
+                    oView.addDependent(oDialog);
+                    return oDialog;
+                });
+            }
+
+            this._pDeptDialog.then(function (oDialog) {
+                oDialog.open();
+            });
+        },
+
+        onDeptSearch: function (oEvent) {
+            var sValue = oEvent.getParameter("value");
+            var oFilter = new Filter("departmentName", FilterOperator.Contains, sValue);
+            oEvent.getSource().getBinding("items").filter([oFilter]);
+        },
+
+        onDeptDialogClose: function (oEvent) {
+            var oSelectedItem = oEvent.getParameter("selectedItem");
+            if (oSelectedItem) {
+                var sDeptName = oSelectedItem.getTitle();
+                this.getView().getModel("ui").setProperty("/selectedDepartmentName", sDeptName);
+            }
+            oEvent.getSource().getBinding("items").filter([]);
+        },
+
+        // Supplier SelectDialog
+        onSuppHelpRequest: function () {
+            var oView = this.getView();
+
+            if (!this._pSuppDialog) {
+                this._pSuppDialog = Fragment.load({
+                    id: oView.getId(),
+                    name: "project4.fragment.SupplierDialog",
+                    controller: this
+                }).then(function (oDialog) {
+                    oView.addDependent(oDialog);
+                    return oDialog;
+                });
+            }
+
+            this._pSuppDialog.then(function (oDialog) {
+                oDialog.open();
+            });
+        },
+
+        onSuppSearch: function (oEvent) {
+            var sValue = oEvent.getParameter("value");
+            var oFilter = new Filter("name", FilterOperator.Contains, sValue);
+            oEvent.getSource().getBinding("items").filter([oFilter]);
+        },
+
+        onSuppDialogClose: function (oEvent) {
+            var oSelectedItem = oEvent.getParameter("selectedItem");
+            if (oSelectedItem) {
+                var sSuppName = oSelectedItem.getTitle();
+                this.getView().getModel("ui").setProperty("/selectedSuppname", sSuppName);
+            }
+            oEvent.getSource().getBinding("items").filter([]);
         }
-      });
-    },
-
-    onCancel: function () {
-      var oRouter = this.getOwnerComponent().getRouter();
-      oRouter.navTo("ListPage");
-    }
-
-  });
+    });
 });
